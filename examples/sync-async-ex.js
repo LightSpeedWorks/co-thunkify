2b83d4a296691374b5962eb571c9d728ca04c6d3
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2014-04-05 22:07:56 +0900

    modify sync and async example

--- examples/sync-async-ex.js
+++ examples/sync-async-ex.js
@@ -12,47 +12,65 @@ function tm() {
 // sync - 同期
 var sync = thunkify(function (a, b, cb) {
   cb(null, a + b);
-  cb(null, b - a);  // 2nd callback ignored
+  cb(null, b - a);  // 2nd callback result ignored
 });
 
-sync(12, 34)(function (err, result) {
-  console.log('sync 1: result =', result, tm());
+var th0 = sync(1, 2)
+th0(function (err, result) {
+  console.log('sync 1: result =', result, tm(), '(1)');
+});
+th0(function (err, result) {
+  console.log('sync 1: result =', result, tm(), '(2)');
 });
 
-var th = sync(12, 34);
-th(function (err, result) {
-  console.log('sync 2: result =', result, tm());
+var th1 = sync(2, 3);
+th1(function (err, result) {
+  console.log('sync 2: result =', result, tm(), '(1)');
 });
-th(function (err, result) {
-  console.log('sync 2: ignored =', result, tm());  // 2nd thunk ignored
+th1(function (err, result) {
+  console.log('sync 2: result =', result, tm(), '(2)');
 });
 
 // async - 非同期
 var async = thunkify(function (a, b, cb) {
   setTimeout(function () { cb(null, a + b); }, 400);
-  setTimeout(function () { cb(null, b - a); }, 600);  // 2nd callback ignored
+  setTimeout(function () { cb(null, b - a); }, 600);  // 2nd callback result ignored
 });
 
-async(12, 34)(function (err, result) {
-  console.log('async 1: result =', result, tm());
+var th2 = async(3, 4);
+th2(function (err, result) {
+  console.log('async 1: result =', result, tm(), '(1)');
+});
+th2(function (err, result) {
+  console.log('async 1: result =', result, tm(), '(2)');
 });
 
-var th = async(12, 34);
+var th3 = async(4, 5);
 setTimeout(function () {
-  th(function (err, result) {
-    console.log('async 2: result =', result, tm());
+  th3(function (err, result) {
+    console.log('async 2: result =', result, tm(), '(1)');
   });
-  th(function (err, result) {
-    console.log('async 2: ignored =', result, tm());  // 2nd thunk ignored
+  th3(function (err, result) {
+    console.log('async 2: result =', result, tm(), '(2)');
   });
 }, 200);
 
-var th = async(12, 34);
+var th4 = async(5, 6);
 setTimeout(function () {
-  th(function (err, result) {
-    console.log('async 3: result =', result, tm());
+  th4(function (err, result) {
+    console.log('async 3: result =', result, tm(), '(1)');
   });
-  th(function (err, result) {
-    console.log('async 3: ignored =', result, tm());  // 2nd thunk ignored
+  th4(function (err, result) {
+    console.log('async 3: result =', result, tm(), '(2)');
+  });
+}, 500);
+
+var th5 = async(6, 7);
+setTimeout(function () {
+  th5(function (err, result) {
+    console.log('async 4: result =', result, tm(), '(1)');
+  });
+  th5(function (err, result) {
+    console.log('async 4: result =', result, tm(), '(2)');
   });
 }, 800);
