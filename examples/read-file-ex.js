2b764c46fa73cef9c10c80652fa12e6c6e9adcee
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2014-04-05 22:02:37 +0900

    rename file name to read

--- examples/read-file-ex.js
+++ examples/read-file-ex.js
@@ -8,7 +8,7 @@ var fs = require('fs');
 
 // read file - ファイル読み込み
 var readFile = thunkify(fs.readFile);
-readFile('read-file-ex.js', 'utf8')(function (err, buff) {
+readFile('README.md', 'utf8')(function (err, buff) {
   var str = buff.toString();
   // str...
   console.log(str);
