1cf371600dd11a16c251bdf0ce6bc8d4fa4c8792
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2014-06-04 19:10:58 +0900

    add examples for yieldable promise

---
+++ examples/random-promise-ex.js
@@ -0,0 +1,63 @@
+'use strict';
+
+try {
+  var thunkify = require('../lib/thunkify');
+} catch(err) {
+  console.log('../lib/thunkify ' + err);
+  var thunkify = require('co-thunkify');
+}
+
+// sleep - 眠る
+var sleep = thunkify(function (ms, cb) {
+  setTimeout(cb, ms);
+});
+
+var n = 0;
+
+// random - 眠る
+var random = thunkify(function (v, cb) {
+  var m = 0;
+  ++n;
+  setTimeout(function () {
+    if (m++ === 0) console.log('s:' + v + n); cb(null, 'suc: ' + v + n);
+  }, Math.random() * 100 + 100);
+  setTimeout(function () {
+    if (m++ === 0) console.log('e:' + v + n); cb(new Error('err ' + v + n));
+  }, Math.random() * 100 + 100);
+});
+
+console.log('random start...');
+random('ax')
+.then(function (res) {
+  console.log('1 success... ' + res);
+  return random('bx');
+}, function (err) {
+  console.log('1 error... ' + err);
+  return random('by');
+})
+.then(function (res) {
+  console.log('2 success... ' + res);
+  return random('cx');
+}, function (err) {
+  console.log('2 error... ' + err);
+  return random('cy');
+})
+.then(function (res) {
+  console.log('3 success... ' + res);
+  return random('dx');
+}, function (err) {
+  console.log('3 error... ' + err);
+  return random('dy');
+})
+.catch(function (err) {
+  console.log('4 error... ' + err);
+  return random('ey');
+})
+.then(function (res) {
+  console.log('5 success... ' + res);
+  return random('fx');
+}, function (err) {
+  console.log('5 error... ' + err);
+  return random('fy');
+});
+
---
+++ examples/sleep-promise-ex.js
@@ -0,0 +1,20 @@
+'use strict';
+
+try {
+  var thunkify = require('../lib/thunkify');
+} catch(err) {
+  console.log('../lib/thunkify ' + err);
+  var thunkify = require('co-thunkify');
+}
+
+// sleep - 眠る
+var sleep = thunkify(function (ms, cb) {
+  setTimeout(cb, ms);
+});
+
+console.log('sleep start...');
+sleep(3000).then(function () {
+  console.log('3 seconds later...');
+}).catch(function () {
+  console.log('err?');
+});
